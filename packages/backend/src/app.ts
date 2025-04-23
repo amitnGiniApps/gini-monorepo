ddd758fbd909780306d2d3d64ca219a269d54ec4
pinis-gini-apps <pinis@gini-apps.com>
2025-04-23 21:02:08 +0300

    added projects in backend

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+const path = require('path');
+
 
 import MessageResponse from './interfaces/MessageResponse';
 import gptRouter from './routes/gptRouter';
@@ -15,13 +17,45 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
+app.use('/api/v2', gptRouter);
+
+
+app.get('/api/v1/generate/map', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/map1.html'));
 });
 
-app.use('/api/v1', gptRouter);
+app.get('/api/v1/generate/map2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/map2.html'));
+});
+
+app.get('/api/v1/generate/map3', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/map3.html'));
+});
+
+app.get('/api/v1/generate/calendar', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/calendar.html'));
+});
+
+app.get('/api/v1/generate/dashboard1', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/dashboard-1.html'));
+});
+
+app.get('/api/v1/generate/dashboard2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/dashboard-2.html'));
+});
+
+
+app.get('/api/v1/generate/chat1', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/chat-conversation-1.html'));
+});
+
+app.get('/api/v1/generate/chat2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/chat-conversation-2.html'));
+});
+
+app.get('/api/v1/generate/track', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/track.html'));
+});
 
 
 export default app;
--- packages/backend/src/routes/gptRouter.ts
+++ packages/backend/src/routes/gptRouter.ts
@@ -3,7 +3,7 @@ import { generateProjectController } from '../controllers/gptController';
 
 const router = express.Router();
 
-router.route('/generate').post(generateProjectController);
+router.route('/generate').get(generateProjectController);
 
 export default router;
 
